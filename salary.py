f75a9014ca6ef9151d1ff31890c7b37e43a43a60
aliHdedeh-dev <hdedehali@gmail.com>
2025-02-20 01:06:42 +0200

    asking nabiha for inputs

---
+++ salary.py
@@ -0,0 +1,6 @@
+salary = float(input("Please enter your salary: "))
+month = input("Please enter the month: ")
+savings_percentage = float(input("Please enter the percentage of salary to save: "))
+rent_percentage = float(input("Please enter the percentage of salary to spend on rent: "))
+electricity_percentage = float(input("Please enter the percentage of salary to spend on electricity: "))
+
