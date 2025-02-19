c2e269d191f7f282a4e248becbcbd75c7669378b
aliHdedeh-dev <hdedehali@gmail.com>
2025-02-20 01:10:55 +0200

    calculating the amount allocated to rent,savings, electricity

--- salary.py
+++ salary.py
@@ -4,3 +4,8 @@ savings_percentage = float(input("Please enter the percentage of salary to save:
 rent_percentage = float(input("Please enter the percentage of salary to spend on rent: "))
 electricity_percentage = float(input("Please enter the percentage of salary to spend on electricity: "))
 
+savings_amount = salary * savings_percentage / 100
+rent_amount = salary * rent_percentage / 100
+electricity_amount = salary * electricity_percentage / 100
+
+
